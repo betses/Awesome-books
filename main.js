bed44ae5937f5d2a16e69e0dbb1490af44eda1c6
niicode <niidarku66@gmail.com>
2022-08-01 09:36:27 +0000

    add render and remove functionality

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 const collections = [];
-// const Book = {
-//   title: '',
-//   author: '',
-// };
+const bookContainer = document.querySelector('.book-container');
+const AddBookForm = document.querySelector('.book-form');
 
-// const firstBook = Book;
-// firstBook.add('sasss', 'lorem ipsum');
-// collections.push(firstBook);
-// console.log(collections);
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
 
 function addBook(title, author) {
   const book = {
@@ -16,16 +12,49 @@ function addBook(title, author) {
   collections.push(book);
 }
 
-function removeBook(title) {
-  // eslint-disable-next-line array-callback-return, consistent-return
-  collections.filter((book) => {
-    if (book.title === title) {
-      return book.title !== title;
-    }
+
+// [1,2,3,4,5,6,7,8,9,10]
+
+function removeBook(index) {
+  collections.splice(index, 1);
+}
+
+function render() {
+  bookContainer.innerHTML = '';
+  collections.forEach((book, index) => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.innerHTML = `
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <button data-remove=${index} class="delete">Remove</button>
+      <hr>
+    `;
+    bookContainer.appendChild(bookElement);
+
+    const deleteButton = bookElement.querySelector('.delete');
+    deleteButton.addEventListener('click', () => {
+      removeBook(index);
+      render();
+    });
+
   });
 }
 
-addBook('asssa', 'jajaja');
-addBook('book2', 'author2');
-removeBook('asssa');
-console.log(collections);
+AddBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  addBook(title, author);
+  bookTitle.value = '';
+  bookAuthor.value = '';
+  render();
+})
+
+
+// const removeButton = document.querySelector('.delete');
+// removeButton.addEventListener('click', (e) => {
+//   const index = e.target.dataset.remove;
+//   removeBook(index);
+//   console.log(index);
+// })
